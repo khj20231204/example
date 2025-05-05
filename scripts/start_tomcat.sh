6d7cb9802916ccf384d4515fa85f931c9c37fc9d
khj <natista00@naver.com>
2025-05-05 09:32:56 +0900

    before2 id pw

--- scripts/start_tomcat.sh
+++ scripts/start_tomcat.sh
@@ -4,7 +4,7 @@ rm -f /usr/local/tomcat/webapps/ROOT.war
 rm -rf /usr/local/tomcat/webapps/ROOT
 
 # 새로운 WAR 파일 이동
-mv /usr/local/tomcat/webapps/myapp.war /usr/local/tomcat/webapps/ROOT.war
+cp /usr/local/tomcat/webapps/myapp.war /usr/local/tomcat/webapps/ROOT.war
 
 # Tomcat 재시작 (시스템에 따라 명령어가 다를 수 있음)
 sudo systemctl restart tomcat
